e4a55f46e89e279d22b9259164a9f2ff542af377
Paul Stüber <paul.stueber@web.de>
2013-02-21 16:47:27 +0100

    added longin test, changed visualisation test to first login

--- test/features/mindmap/node/LoginTest.java
+++ test/features/mindmap/node/LoginTest.java
@@ -1,61 +1,54 @@
 package features.mindmap.node;
 
-import base.DocearHttpTest;
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import play.libs.F;
+import play.test.TestBrowser;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.google.common.base.Predicate;
-import org.junit.Test;
-import play.libs.F;
-import play.test.TestBrowser;
 
-import javax.annotation.Nullable;
+import base.DocearHttpTest;
 
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.fest.assertions.fluentlenium.FluentLeniumAssertions.assertThat;
-import static org.fest.assertions.Assertions.assertThat;
-
-public class VisualizationTest extends DocearHttpTest {
+public class LoginTest extends DocearHttpTest {
 
     @Test
-    public void testText() throws Exception {
+    public void loginLogoutTest() throws Exception {
         runInBrowser(new F.Callback<TestBrowser>() {
             @Override
             @SuppressWarnings("unchecked") //type system is in fluentlenium library broken
-            public void invoke(final TestBrowser testBrowser) throws Throwable {     		
-            	WebClient webClient = webClient();
-
+            public void invoke(final TestBrowser testBrowser) throws Throwable {            	WebClient webClient = webClient();
 				HtmlPage page = webClient.getPage(url("/"));
 				webClient.waitForBackgroundJavaScriptStartingBefore(defaultWait() * 1000);
-
+	
 				HtmlForm form = page.getFirstByXPath("//form[@id='login-form']");
 				
 				HtmlInput username = form.getFirstByXPath(".//input[@id='username']");
 				username.type("Paul");
-
+	
 				HtmlPasswordInput password = form.getFirstByXPath(".//input[@id='password']");
 				password.type("secret");
 				
 				HtmlButton login = form.getFirstByXPath(".//button[@type='submit']");
-				
 				page = login.click();
 				webClient.waitForBackgroundJavaScriptStartingBefore(defaultWait() * 1000); // since
 
-				HtmlElement loadMapLink = page.getFirstByXPath("//a[@id='load-map-5']");
-				page = loadMapLink.click();
+				assertThat(page.asText()).contains("Logout");
 				
-				webClient.waitForBackgroundJavaScriptStartingBefore(defaultWait() * 1000); // since
-
-				HtmlElement mindmapContainer = page.getFirstByXPath("//div[@id='mindmap']");
+				HtmlAnchor logout = page.getFirstByXPath("//a[@href='/logout']");
+				page = logout.click();
+				webClient.waitForBackgroundJavaScriptStartingBefore(defaultWait() * 1000);
 				
-				assertThat(mindmapContainer.asText()).contains("right_L2P01_NodeLink_ToParent");
-			}
+				assertThat(page.getElementById("login-form")).isNotNull();
+				
+            }
         });
     }
 }
