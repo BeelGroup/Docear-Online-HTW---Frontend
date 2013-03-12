6f5c2acd3c6390243c012ca59511fb5462aa861e
AlexSchwank <alschwank@gmail.com>
2013-03-12 09:24:47 +0100

    added logging to mindmap controller. changed method of deleteNode

--- app/controllers/MindMap.java
+++ app/controllers/MindMap.java
@@ -23,8 +23,9 @@ public class MindMap extends Controller {
 	private MindMapCrudService mindMapCrudService;
 
 	@Security.Authenticated(Secured.class)
-	public Result map(final String id) throws DocearServiceException, IOException {
-        final F.Promise<String> mindMapPromise = mindMapCrudService.mindMapAsJsonString(id);
+	public Result map(final String mapId) throws DocearServiceException, IOException {
+		Logger.debug("MindMap.map <- mapId="+mapId);
+        final F.Promise<String> mindMapPromise = mindMapCrudService.mindMapAsJsonString(mapId);
 		return async(mindMapPromise.map(new F.Function<String, Result>() {
             @Override
             public Result apply(String mindMap) throws Throwable {
@@ -35,6 +36,7 @@ public class MindMap extends Controller {
     
 	@Security.Authenticated(Secured.class)
     public Result addNode() {
+		Logger.debug("MindMap.addNode <- body="+request().body());
     	JsonNode addNodeJSON = request().body().asJson();
         final F.Promise<JsonNode> addNodePromise = mindMapCrudService.addNode(addNodeJSON);
         return async(addNodePromise.map(new F.Function<JsonNode, Result>() {
@@ -47,6 +49,7 @@ public class MindMap extends Controller {
     
     @Security.Authenticated(Secured.class)
     public Result createNode(final String mapId) {
+    	Logger.debug("MindMap.createNode <- mapId="+mapId+", body="+request().body().asText());
     	Map<String, String[]> bodyEntries = request().body().asFormUrlEncoded();
     	
     	final String parentNodeId = bodyEntries.get("parentNodeId")[0];
@@ -61,6 +64,7 @@ public class MindMap extends Controller {
     
     @Security.Authenticated(Secured.class)
     public Result getNode(final String mapId, final String nodeId) {
+    	Logger.debug("MindMap.createNode <- mapId="+mapId+", nodeId="+nodeId);
         final F.Promise<String> addNodePromise = mindMapCrudService.getNode(mapId, nodeId);
         return async(addNodePromise.map(new F.Function<String, Result>() {
             @Override
@@ -72,17 +76,18 @@ public class MindMap extends Controller {
     
     @Security.Authenticated(Secured.class)
     public Result changeNode(final String mapId) {
+    	Logger.debug("MindMap.changeNode <- mapId="+mapId+", body="+request().body().asText());
     	Map<String, String[]> bodyEntries = request().body().asFormUrlEncoded();
     	
     	final String nodeJson = bodyEntries.get("nodeJson")[0];
-    	Logger.debug("changeNode => mapId: '"+mapId+"', nodeJson:'"+nodeJson+"'");
     	
     	mindMapCrudService.ChangeNode(mapId, nodeJson);
     	return ok();
     }
     
     @Security.Authenticated(Secured.class)
-    public Result deleteNode(String mapId) {
+    public Result deleteNode(final String mapId, final String nodeId) {
+    	Logger.debug("MindMap.deleteNode <- mapId="+mapId+", nodeId="+nodeId);
     	return TODO;
     }
 }
