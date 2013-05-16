64c19705b1f87b4988e2c9ca84f52912edcfaa6c
Julius Seltenheim <mail@julius-seltenheim.com>
2013-05-16 12:57:10 +0200

    #280, added API-Server route and handling for changeEdge

---
+++ app/models/frontend/formdata/ChangeEdgeData.java
@@ -0,0 +1,27 @@
+package models.frontend.formdata;
+
+import play.data.validation.Constraints.Required;
+
+public class ChangeEdgeData {
+	@Required
+	private String nodeId;
+
+	@Required
+	private String attributeValueMapJson;
+
+	public String getNodeId() {
+		return nodeId;
+	}
+
+	public void setNodeId(String nodeId) {
+		this.nodeId = nodeId;
+	}
+
+	public String getAttributeValueMapJson() {
+		return attributeValueMapJson;
+	}
+
+	public void setAttributeValueMapJson(String attributeValueMapJson) {
+		this.attributeValueMapJson = attributeValueMapJson;
+	}
+}
